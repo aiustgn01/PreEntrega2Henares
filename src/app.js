3e2d6bf03cd7e1fbce764e152d9ec0d8d173dcf3
aiustgn01 <agustinnhenares@gmail.com>
2023-09-18 07:19:02 -0300

    React 1.0

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/navbar';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+     <h1>Bienvenidos a mi primer intento de hacer una pagina con react!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav class='navbar'>
+      
+        <a href="/" class='boton'>Inicio</a>
+        <a href="/acerca" class='boton'>Acerca de</a>
+        <a href="/contacto" class='boton'>Contacto</a>
+      
+    </nav>
+  );
+}
+
+export default Navbar;
