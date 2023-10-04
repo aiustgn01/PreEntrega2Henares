221959d07a481fe6ff62d7f3eb55d9b11ce45205
aiustgn01 <agustinnhenares@gmail.com>
2023-10-03 23:44:19 -0300

    Signed-off-by: aiustgn01 <agustinnhenares@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './app.css';
 import Navbar from './components/navbar';
 import ItemListContainer from './components/ItemListContainer';
-
-
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+export default function App() {
   const saludo='holis'
-  return (
-    <div>
-      <Navbar hola='hola'/>
-      <ItemListContainer greeting='Bienvenidos a PRUNE' saludo={saludo}/>
-     <h1>Bienvenidos a mi primer intento de hacer una pagina con react!</h1>
-    </div>
-  );
+
+  return(
+    <BrowserRouter>
+    <Navbar hola='hola'/>
+    <ItemListContainer greeting='Bienvenidos a PRUNE' saludo={saludo}/>
+    <h1>Bienvenidos a mi primer intento de hacer una pagina con react!</h1>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/pokemons" element={<Pokemons />} />
+      <Route exact path="/pokemon/:pokemonId" element={<Pokemons />} />
+      <Route exact path="/category/:categoryId" element={<Category />} />
+      <Route path="/pokemons/*" element={<h1>Wildcard</h1>} />
+      </Routes>
+      </BrowserRouter>
+  )
 }
 
-export default App;
